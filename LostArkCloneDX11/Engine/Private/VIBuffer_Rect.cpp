0c8efdfa90e64a243e2dc74667fcc52345c682b0
JinhoBa <127280695+JinhoBa@users.noreply.github.com>
2025-07-31 15:47:29 +0900

    feat : Add VIBuffer Component Frame
    - VIBuffer 기본 틀 작성

---
+++ LostArkCloneDX11/Engine/Private/VIBuffer_Rect.cpp
@@ -0,0 +1,131 @@
+#include "VIBuffer_Rect.h"
+
+CVIBuffer_Rect::CVIBuffer_Rect(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+	: CVIBuffer{pDevice, pContext}
+{
+}
+
+CVIBuffer_Rect::CVIBuffer_Rect(CVIBuffer_Rect& Prototype)
+	:CVIBuffer{Prototype}
+{
+}
+
+HRESULT CVIBuffer_Rect::Initialize_Prototype()
+{
+	m_iNumVertexBuffers = 1;
+	m_iNumVertices = 4;
+	m_iVertexStride = sizeof(VTXPOSTEX);
+
+	m_iNumIndices = 6;
+	m_iIndexStride = 2;
+
+	m_eIndexFormat = DXGI_FORMAT_R16_UINT;
+	m_ePrimitive = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+
+#pragma region VETEX_BUFFER
+	D3D11_BUFFER_DESC VBDesc = {};
+	VBDesc.ByteWidth = m_iVertexStride * m_iNumVertices;
+	VBDesc.Usage = D3D11_USAGE_DEFAULT;
+	VBDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	VBDesc.StructureByteStride = m_iVertexStride;
+	VBDesc.CPUAccessFlags = 0;
+	VBDesc.MiscFlags = 0;
+
+	VTXPOSTEX* pVertices = new VTXPOSTEX[m_iNumVertices];
+	ZeroMemory(pVertices, sizeof(VTXPOSTEX) * m_iNumVertices);
+
+	m_pVertexPosition = new _float3[m_iNumVertices];
+	ZeroMemory(m_pVertexPosition, sizeof(_float3) * m_iNumVertices);
+
+	m_pVertexPosition[0] = pVertices[0].vPosition = _float3(-0.5f, 0.5f, 0.f);
+	pVertices[0].vTexcoord = _float2(0.f, 0.f);
+
+	m_pVertexPosition[1] = pVertices[1].vPosition = _float3(0.5f, 0.5f, 0.f);
+	pVertices[1].vTexcoord = _float2(1.f, 0.f);
+
+	m_pVertexPosition[2] = pVertices[2].vPosition = _float3(0.5f, -0.5f, 0.f);
+	pVertices[2].vTexcoord = _float2(1.f, 1.f);
+
+	m_pVertexPosition[3] = pVertices[3].vPosition = _float3(-0.5f, -0.5f, 0.f);
+	pVertices[3].vTexcoord = _float2(0.f, 1.f);
+
+	D3D11_SUBRESOURCE_DATA InitVBData = {};
+	InitVBData.pSysMem = pVertices;
+
+	if (FAILED(m_pDevice->CreateBuffer(&VBDesc, &InitVBData, &m_pVB)))
+		return E_FAIL;
+
+	Safe_Delete_Array(pVertices);
+
+#pragma endregion
+
+#pragma region INDEX_BUFFER
+	D3D11_BUFFER_DESC IBDesc = {};
+	IBDesc.ByteWidth = m_iNumIndices * m_iIndexStride;
+	IBDesc.Usage = D3D11_USAGE_DEFAULT;
+	IBDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	IBDesc.StructureByteStride = m_iIndexStride;
+	IBDesc.CPUAccessFlags = 0;
+	IBDesc.MiscFlags = 0;
+
+	_ushort* pIndices = new _ushort[m_iNumIndices];
+	ZeroMemory(pIndices, sizeof(_ushort) * m_iNumIndices);
+
+	pIndices[0] = 0;
+	pIndices[1] = 1;
+	pIndices[2] = 2;
+
+	pIndices[3] = 0;
+	pIndices[4] = 2;
+	pIndices[5] = 3;
+
+	D3D11_SUBRESOURCE_DATA InitIBData = {};
+	InitIBData.pSysMem = pIndices;
+
+	if (FAILED(m_pDevice->CreateBuffer(&IBDesc, &InitIBData, &m_pVB)))
+		return E_FAIL;
+
+	Safe_Delete_Array(pIndices);
+#pragma endregion
+
+
+	return S_OK;
+}
+
+HRESULT CVIBuffer_Rect::Initialize(void* pArg)
+{
+	return S_OK;
+}
+
+CVIBuffer_Rect* CVIBuffer_Rect::Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
+{
+	CVIBuffer_Rect* pInstance = new CVIBuffer_Rect(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize_Prototype()))
+	{
+		Safe_Release(pInstance);
+		MSG_BOX("Failed to Create : CVIBuffer_Rect");
+		return nullptr;
+	}
+
+	return pInstance;
+}
+
+CComponent* CVIBuffer_Rect::Clone(void* pArg)
+{
+	CComponent* pInstance = new CVIBuffer_Rect(*this);
+
+	if (FAILED(pInstance->Initialize(pArg)))
+	{
+		Safe_Release(pInstance);
+		MSG_BOX("Failed to Clone : CVIBuffer_Rect");
+		return nullptr;
+	}
+
+	return pInstance;
+}
+
+void CVIBuffer_Rect::Free()
+{
+	__super::Free();
+}
