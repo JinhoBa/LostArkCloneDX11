f43149475d476f910362acc9fb3cb8b8c961b01b
JinhoBa <127280695+JinhoBa@users.noreply.github.com>
2025-08-10 01:17:52 +0900

    feat : Picking
    - Picking class 추가
    - 마우스의 위치, 방향을 World, Local Space로 내려서 업데이트마다 한번만 수행하도록 한다.

---
+++ LostArkCloneDX11/Engine/Public/Picking.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Base.h"
+
+NS_BEGIN(Engine)
+
+class CPicking final : public CBase
+{
+private:
+	CPicking(ID3D11Device* pDevice, ID3D11DeviceContext* pContext);
+	virtual ~CPicking() = default;
+
+public:
+	HRESULT Initialize(HWND hWnd);
+	void Update();
+	
+	void Transform_ToLocalSpace(const FXMMATRIX pWorldMatrixInverse);
+
+	_bool Picking_InWorldSpace(const FXMVECTOR vPointA, const FXMVECTOR vPointB, const FXMVECTOR vPointC, _float3* pQut);
+	_bool Picking_InLocalSpace(const FXMVECTOR vPointA, const FXMVECTOR vPointB, const FXMVECTOR vPointC, _float3* pQut);
+
+private:
+	ID3D11Device*			m_pDevice = { nullptr };
+	ID3D11DeviceContext*	m_pContext = { nullptr };
+	class CGameInstance*	m_pGameInstance = { nullptr };
+
+	HWND					m_hWnd = {};
+	_float					m_fWinSizeX{}, m_fWinSizeY{};
+
+	_float3					m_vRayDir[ENUM_TO_INT(RAY::END)];
+	_float3					m_vRayPos[ENUM_TO_INT(RAY::END)];
+
+public:
+	static CPicking* Create(ID3D11Device* pDevice, ID3D11DeviceContext* pContext, HWND hWnd);
+	virtual void Free() override;
+
+};
+
+NS_END
